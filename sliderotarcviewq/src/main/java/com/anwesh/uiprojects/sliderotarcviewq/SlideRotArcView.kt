e80e9bbadb73160b613a22f1c02989ecb7c3fe36
Anwesh43 <anweshthecool0@gmail.com>
2019-06-01 12:57:45 +0530

    adding drawSRANode canvas extension method

--- sliderotarcviewq/src/main/java/com/anwesh/uiprojects/sliderotarcviewq/SlideRotArcView.kt
+++ sliderotarcviewq/src/main/java/com/anwesh/uiprojects/sliderotarcviewq/SlideRotArcView.kt
@@ -22,6 +22,7 @@ val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#311B92")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rotDeg : Float = 45f
+val rFactor : Float = 3.6f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -32,3 +33,34 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) / a + k / b
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawLineArc(i : Int, sc1 : Float, sc2 : Float, size : Float, paint : Paint) {
+    val r : Float = size / rFactor
+    val y : Float = -size * sc1
+    save()
+    rotate(rotDeg * sc2.divideScale(i, lines))
+    drawLine(0f, 0f, 0f, y, paint)
+    save()
+    translate(0f, y)
+    drawArc(RectF(-r, -r, r, r), 180f * i, 180f, true, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawSRANode(i : Int, scale : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, gap * (i + 1))
+    for (j in 0..(lines - 1)) {
+        drawLineArc(j, sc1, sc2, size, paint)
+    }
+    restore()
+}
