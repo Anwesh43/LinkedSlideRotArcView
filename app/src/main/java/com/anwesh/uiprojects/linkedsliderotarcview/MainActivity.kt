f932c85635f614abbada61ee13491ec25344e958
Anwesh43 <anweshthecool0@gmail.com>
2019-06-01 14:17:18 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedsliderotarcview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsliderotarcview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedsliderotarcview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.sliderotarcviewq.SlideRotArcView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SlideRotArcView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
